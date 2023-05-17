5f5f987116ee00dbeaf2452643d1b3458beeb0e8
mavo <lllmavolll@gmail.com>
2023-05-17 17:30:51 +0900

    目玉の親父

--- js/script.js
+++ js/script.js
@@ -1,18 +1,16 @@
 const eyeEl = document.getElementById("eye");
-const svgEl = document.getElementById("svg");
-const svgPos = svgEl.getBoundingClientRect();
 const eyePos = eyeEl.getBoundingClientRect();
 
-const cx = eyePos.x - svgPos.x + eyePos.width / 2;
-const cy = eyePos.y - svgPos.y + eyePos.height / 2 + 0.5;
+const cx = +eyeEl.getAttribute("cx");
+const cy = +eyeEl.getAttribute("cy");
 let dx, dy, rad;
 let mouseTimer;
 
 addEventListener("mousemove", e => {
   clearTimeout(mouseTimer);
 
-  dx = e.clientX - eyePos.x + eyePos.width / 2;
-  dy = e.clientY - eyePos.y + eyePos.height / 2;
+  dx = e.clientX - eyePos.x - eyePos.width / 2;
+  dy = e.clientY - eyePos.y - eyePos.height / 2;
   rad = Math.atan2(dy, dx);
 
   eyeEl.setAttribute("cx", 4 * Math.cos(rad) + cx);
