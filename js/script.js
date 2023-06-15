916a32e0c6798aaa859d7776c838db45948bf0e3
mavo <lllmavolll@gmail.com>
2023-06-15 21:58:00 +0900

    めんたま

--- js/script.js
+++ js/script.js
@@ -1,23 +1,24 @@
-const eyeEl = document.getElementById("eye");
-const eyePos = eyeEl.getBoundingClientRect();
+(() => {
+  const eyeEl = document.getElementById("eye");
+  const eyePos = eyeEl.getBoundingClientRect();
 
-const cx = +eyeEl.getAttribute("cx");
-const cy = +eyeEl.getAttribute("cy");
-let dx, dy, rad;
-let mouseTimer;
+  const cx = +eyeEl.getAttribute("cx");
+  const cy = +eyeEl.getAttribute("cy");
 
-addEventListener("mousemove", e => {
-  clearTimeout(mouseTimer);
+  let mouseTimer;
+  addEventListener("mousemove", e => {
+    clearTimeout(mouseTimer);
 
-  dx = e.clientX - eyePos.x - eyePos.width / 2;
-  dy = e.clientY - eyePos.y - eyePos.height / 2;
-  rad = Math.atan2(dy, dx);
+    let dx = e.clientX - eyePos.x - eyePos.width / 2;
+    let dy = e.clientY - eyePos.y - eyePos.height / 2;
+    let rad = Math.atan2(dy, dx);
 
-  eyeEl.setAttribute("cx", 4 * Math.cos(rad) + cx);
-  eyeEl.setAttribute("cy", 5 * Math.sin(rad) + cy);
+    eyeEl.setAttribute("cx", 4 * Math.cos(rad) + cx);
+    eyeEl.setAttribute("cy", 5 * Math.sin(rad) + cy);
 
-  mouseTimer = setTimeout(() => {
-    eyeEl.setAttribute("cx", cx);
-    eyeEl.setAttribute("cy", cy);
-  }, 500);
-});
+    mouseTimer = setTimeout(() => {
+      eyeEl.setAttribute("cx", cx);
+      eyeEl.setAttribute("cy", cy);
+    }, 500);
+  });
+})();
---
+++ js/script2.js
@@ -0,0 +1,103 @@
+(() => {
+  const leftPupilEl = document.getElementById("leftPupil");
+  const rightPupilEl = document.getElementById("rightPupil");
+  const leftEyelidEL = document.getElementById("leftEyelid");
+  const rightEyelidEL = document.getElementById("rightEyelid");
+  // const leftEyeEl = document.getElementById("leftEye");
+  // const rightEyeEl = document.getElementById("rightEye");
+
+  const email = document.getElementById("email");
+  const emailStyle = getComputedStyle(email);
+  const password = document.getElementById("password");
+
+  const rightEyePos = rightPupilEl.getBoundingClientRect();
+  const rightEyeCx = rightEyePos.x + rightEyePos.width / 2;
+  const rightEyeCy = rightEyePos.y + rightEyePos.height / 2;
+  const leftEyePos = leftPupilEl.getBoundingClientRect();
+  const leftEyeCx = leftEyePos.x + leftEyePos.width / 2;
+  const leftEyeCy = leftEyePos.y + leftEyePos.height / 2;
+
+  const cx = +leftPupilEl.getAttribute("cx");
+  const cy = +leftPupilEl.getAttribute("cy");
+
+  email.addEventListener("input", getCoordinates);
+  email.addEventListener("focus", getCoordinates);
+  email.addEventListener("blur", () => {
+    rightPupilEl.setAttribute("cx", cx);
+    rightPupilEl.setAttribute("cy", cy);
+
+    leftPupilEl.setAttribute("cx", cx);
+    leftPupilEl.setAttribute("cy", cy);
+  });
+
+  password.addEventListener("focus", () => {
+    leftEyelidEL.setAttribute("stroke-opacity", 1);
+    rightEyelidEL.setAttribute("stroke-opacity", 1);
+
+    rightPupilEl.setAttribute("stroke-opacity", 0);
+    leftPupilEl.setAttribute("stroke-opacity", 0);
+  });
+  password.addEventListener("blur", () => {
+    leftEyelidEL.setAttribute("stroke-opacity", 0);
+    rightEyelidEL.setAttribute("stroke-opacity", 0);
+
+    rightPupilEl.setAttribute("stroke-opacity", 1);
+    leftPupilEl.setAttribute("stroke-opacity", 1);
+  });
+
+  function getCoordinates(e) {
+    const span = document.createElement("span");
+    const div = document.createElement("div");
+
+    [].forEach.call(emailStyle, function (prop) {
+      div.style[prop] = emailStyle[prop];
+    });
+    div.style.position = "absolute";
+
+    div.textContent = e.target.value;
+    span.textContent = ".";
+    document.body.appendChild(div);
+    div.appendChild(span);
+
+    const emailPos = getPosition(email);
+    const caretPos = getPosition(span);
+
+    const dx = emailPos.x + caretPos.x;
+    const dy = emailPos.y + email.clientHeight / 2;
+    const rightRad = Math.atan2(dy - rightEyeCy, dx - rightEyeCx);
+    const leftRad = Math.atan2(dy - leftEyeCy, dx - leftEyeCx);
+
+    rightPupilEl.setAttribute("cx", 43 * Math.cos(rightRad) + cx);
+    rightPupilEl.setAttribute("cy", 43 * Math.sin(rightRad) + cy);
+    leftPupilEl.setAttribute("cx", 43 * Math.cos(leftRad) + cx);
+    leftPupilEl.setAttribute("cy", 43 * Math.sin(leftRad) + cy);
+
+    document.body.removeChild(div);
+  }
+
+  function getPosition(el) {
+    var xPos = 0;
+    var yPos = 0;
+
+    while (el) {
+      if (el.tagName == "BODY") {
+        // deal with browser quirks with body/window/document and page scroll
+        var xScroll = el.scrollLeft || document.documentElement.scrollLeft;
+        var yScroll = el.scrollTop || document.documentElement.scrollTop;
+
+        xPos += el.offsetLeft - xScroll + el.clientLeft;
+        yPos += el.offsetTop - yScroll + el.clientTop;
+      } else {
+        // for all other non-BODY elements
+        xPos += el.offsetLeft - el.scrollLeft + el.clientLeft;
+        yPos += el.offsetTop - el.scrollTop + el.clientTop;
+      }
+
+      el = el.offsetParent;
+    }
+    return {
+      x: xPos,
+      y: yPos,
+    };
+  }
+})();
