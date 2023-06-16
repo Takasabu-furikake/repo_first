97cb8e0e4a21a87137edebed0a57d3a0de37bbde
mavo <lllmavolll@gmail.com>
2023-06-16 16:22:28 +0900

    めんたま

--- js/script2.js
+++ js/script2.js
@@ -66,8 +66,8 @@
     document.body.appendChild(div);
     div.appendChild(span);
 
-    const emailPos = getPosition(email);
-    const caretPos = getPosition(span);
+    const emailPos = email.getBoundingClientRect();
+    const caretPos = span.getBoundingClientRect();
 
     const dx = emailPos.x + caretPos.x;
     const dy = emailPos.y + email.clientHeight / 2;
@@ -80,31 +80,5 @@
     leftPupilEl.setAttribute("cy", 43 * Math.sin(leftRad) + cy);
 
     document.body.removeChild(div);
-  }
-
-  function getPosition(el) {
-    var xPos = 0;
-    var yPos = 0;
-
-    while (el) {
-      if (el.tagName == "BODY") {
-        // deal with browser quirks with body/window/document and page scroll
-        var xScroll = el.scrollLeft || document.documentElement.scrollLeft;
-        var yScroll = el.scrollTop || document.documentElement.scrollTop;
-
-        xPos += el.offsetLeft - xScroll + el.clientLeft;
-        yPos += el.offsetTop - yScroll + el.clientTop;
-      } else {
-        // for all other non-BODY elements
-        xPos += el.offsetLeft - el.scrollLeft + el.clientLeft;
-        yPos += el.offsetTop - el.scrollTop + el.clientTop;
-      }
-
-      el = el.offsetParent;
-    }
-    return {
-      x: xPos,
-      y: yPos,
-    };
   }
 })();
